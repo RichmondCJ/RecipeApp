30ba3bc65cb5ffe4ab515ac11f7d211d1390b2f0
Chris Richmond <RichmondCJ@gmail.com>
2021-05-01 06:00:53 -0400

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Mac Mini on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
